277074ce5abcb20e262c00277c1496eacbd94df8
user <user@localhost>
2015-09-30 00:00:00 +0000

    Fixed release scripts

--- scripts/release.bash
+++ scripts/release.bash
@@ -7,7 +7,9 @@ if [ "$1" = "version" ] && [ ! -z "$2" ]; then
     git tag "$2"
     perl -p -i -e "s|<<version>>|$2|" version/version.go
     perl -p -i -e "s|<<rev>>|$(git rev-parse HEAD)|" version/version.go
-    tar czf "$root"/irc-workflow-"$2".tgz .
+    curr_date="$(date +%s)"
+    tar czf "$root"/../${curr_date}-irc-workflow-"$2".tgz .
+    mv "$root"/../${curr_date}-irc-workflow-"$2".tgz ./irc-workflow-"$2".tgz
 else
     echo "Usage: $0 version <num>" 1>&2
     exit 1
