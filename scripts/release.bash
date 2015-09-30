c555bbb9aa3e6d406e5331bca0c557c4a5e5a5cb
user <user@localhost>
2015-09-30 00:00:00 +0000

    Testing release scripts

--- scripts/release.bash
+++ scripts/release.bash
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+set -e
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/bootstrap"
 
 if [ "$1" = "version" ] && [ ! -z "$2" ]; then
+    git tag "$2"
     perl -p -i -e "s|<<version>>|$2|" version/version.go
     perl -p -i -e "s|<<rev>>|$(git rev-parse HEAD)|" version/version.go
+    tar czf "$root"/irc-workflow-"$2".tgz .
 else
     echo "Usage: $0 version <num>" 1>&2
     exit 1
